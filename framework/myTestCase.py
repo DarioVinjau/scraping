7679de4add499f882027a1bc7323a6f9641e9be7
Benedetto Marzona <benedetto.marzona@alten.it>
2023-11-06 12:34:50 +0100

    add banner closing click on setUp

--- framework/myTestCase.py
+++ framework/myTestCase.py
@@ -1,6 +1,7 @@
 import unittest
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 class AppTesting(unittest.TestCase):
    
@@ -8,6 +9,7 @@ class AppTesting(unittest.TestCase):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.get("https://www.amazon.it/")
+        self.driver.find_element(By.ID,"sp-cc-accept").click()
         print("setup eseguito")
         
     def tearDown(self):
