e18aa688dca05b88a8470afabbd672f84d8fe0ae
DarioVinjau <dariovinjau@gmail.com>
2024-02-23 17:45:04 +0100

    sistemato pc_page

--- src/pages/home_page.py
+++ src/pages/home_page.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-from base_page import BasePage
+from pages.base_page import BasePage
 #from selenium.webdriver.support.ui import WebDriverWait
 #from selenium.webdriver.support import expected_conditions as EC
 #from selenium.webdriver.common.keys import Keys
