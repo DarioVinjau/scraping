4fd97812844dbb47a6f793a7a9fc7449469a9e27
DarioVinjau <dariovinjau@gmail.com>
2024-02-23 18:02:03 +0100

    sistemato path in webdriver_manager

--- src/webdriver_manager.py
+++ src/webdriver_manager.py
@@ -4,13 +4,15 @@ from selenium.webdriver.chrome.service import Service
 
 
 def get_chrome_driver():
-    absolute_path = os.path.dirname(__file__)
-    abs_replaced = absolute_path.replace("\\","/")
-    relative_path = "/controller/chromedriver.exe"
-    full_path = abs_replaced+relative_path
+    #absolute_path = os.path.dirname(__file__)
+    #abs_replaced = absolute_path.replace("\\","/")
+    #relative_path = "/controller/chromedriver.exe"
+    #full_path = abs_replaced+relative_path
+    path = "src/controller/chromedriver.exe"
     chrome_options = webdriver.ChromeOptions()
     #chrome_options.add_argument("--headless")
     #service = Service(executable_path=relative_path) #"C:/Users/dvinjau/Downloads/chromedriver.exe"
-    service = Service(executable_path="C:/Users/dvinjau/Downloads/chromedriver.exe")
+    #service = Service(executable_path="C:/Users/dvinjau/Downloads/chromedriver.exe")
+    service = Service(executable_path=path)
     driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
